549733b4c6a054a07895d35147034d71e4812ea3
Shiven Khurana <shivenbk@gmail.com>
2026-03-20 09:29:19 -0400

    ed

--- distro/lfs/recipes/base/32-mpfr.sh
+++ distro/lfs/recipes/base/32-mpfr.sh
@@ -7,5 +7,8 @@ recipe_install() {
   local src_dir
   src_dir=$(recipe_unpack "$(recipe_find_source "mpfr-*")")
 
-  recipe_configure_make_install "$src_dir" --prefix=/usr --disable-static
+  CPPFLAGS="-I${LFS_ROOT}/usr/include" \
+  LDFLAGS="-L${LFS_ROOT}/usr/lib" \
+  LD_LIBRARY_PATH="${LFS_ROOT}/usr/lib" \
+  recipe_configure_make_install "$src_dir" --prefix=/usr --disable-static --with-gmp="${LFS_ROOT}/usr"
 }
--- distro/lfs/recipes/base/33-mpc.sh
+++ distro/lfs/recipes/base/33-mpc.sh
@@ -7,5 +7,11 @@ recipe_install() {
   local src_dir
   src_dir=$(recipe_unpack "$(recipe_find_source "mpc-*")")
 
-  recipe_configure_make_install "$src_dir" --prefix=/usr --disable-static
+  CPPFLAGS="-I${LFS_ROOT}/usr/include" \
+  LDFLAGS="-L${LFS_ROOT}/usr/lib" \
+  LD_LIBRARY_PATH="${LFS_ROOT}/usr/lib" \
+  PKG_CONFIG_PATH="${LFS_ROOT}/usr/lib/pkgconfig" \
+  recipe_configure_make_install "$src_dir" --prefix=/usr --disable-static \
+    --with-gmp="${LFS_ROOT}/usr" \
+    --with-mpfr="${LFS_ROOT}/usr"
 }
